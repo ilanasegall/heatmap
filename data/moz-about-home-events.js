d3a26e8d4c72118034b1fa38c901d7d2c78d54b6
Ilana Segall <isegall@mozilla.com>
2013-05-03 03:08:00 -0700

    fix typo w/ timestamps

--- data/moz-about-home-events.js
+++ data/moz-about-home-events.js
@@ -1,9 +1,10 @@
 "use strict";
 
 let searchForm = document.getElementById("searchForm");
+
 searchForm.addEventListener("submit", function() {
 	//click vs enter?
-	self.port.emit("search-form-submit", {search_term:document.getElementById("searchText").value, ts: ts.Date.now()});
+	self.port.emit("search-form-submit", {search_term:document.getElementById("searchText").value, ts: Date.now()});
 }, true);
 
 let widget_ids = ["downloads", "bookmarks", "history", "addons", "sync", "settings"];
@@ -12,6 +13,6 @@ for(let i = 0; i < widget_ids.length; i++) {
 let id = widget_ids[i];
 let widget = document.getElementById(id);
 widget.addEventListener("mouseup", function() {
- 	self.port.emit("homepage-widget-click", {elem:id, ts: ts.Date.now()});
+ 	self.port.emit("homepage-widget-click", {elem:id, ts: Date.now()});
 	}, true);
 }
